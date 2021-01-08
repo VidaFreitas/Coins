a54038ecc3e19d831bbb22aeabe459c75fd531e9
Vida Valessa <vidavalessa65@gmail.com>
2021-01-08 11:17:59 -0300

    Formatting Coins

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace Coins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            decimal valor = 115.25m;
+            Console.WriteLine(valor.ToString("G", CultureInfo.CreateSpecificCulture("en-US")));
+        }
+    }
+}
